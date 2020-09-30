732bc2d4522592b8098cd99c9bf85be04769658f
DankoMaksym <dankomaksym@gmail.com>
2020-10-01 01:16:14 +0300

    feat: imlement math functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    let min = 0;
+    if (!array || !array.length) return min;
 
-exports.min = function min (array) {
-  return 0;
-}
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = 0;
+    if (!array || !array.length) return max;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (!array || !array.length) return sum;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+};
